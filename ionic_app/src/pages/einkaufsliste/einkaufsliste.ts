9fd79cb5704f3bd4b6ed81ceeb1bfbcf78abb3e1
Bob <hanssepp0@gmx.de>
2017-02-28 16:16:44 +0100

    Initial commit einkaufsliste/vorratsliste; UI created; basic utility

--- ionic_app/src/pages/einkaufsliste/einkaufsliste.ts
+++ ionic_app/src/pages/einkaufsliste/einkaufsliste.ts
@@ -6,17 +6,235 @@ import { NavController } from 'ionic-angular';
 
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
-*/
-@Component({
-  selector: 'page-einkaufsliste',
-  templateUrl: 'einkaufsliste.html'
-})
-export class EinkaufslistePage {
+  */
+  @Component({
+  	selector: 'page-einkaufsliste',
+  	templateUrl: 'einkaufsliste.html'
+  })
+  export class EinkaufslistePage {
 
-  constructor(public navCtrl: NavController) {}
+  	vorraetePage: any;
 
-  ionViewDidLoad() {
-    console.log('Hello EinkaufslistePage Page');
+  	newGrocery:any;
+  	newAmount:any;
+
+  	item:any;
+  	item2:any;
+  	item3:any;
+
+  	newItem:any;
+  	currentId:any;
+  	itemlist:Array<any>
+	unitlist: string[] = ["mg", "g", "kg", "l", "liter","Stangen"];
+  	grocerylist: string[] = [];
+  	supplylist:Array<any>;
+
+  	//Test data
+  	neededGroceries:Array<any>;
+  	missingGroceries:Array<any>;
+
+  	constructor(public navCtrl: NavController) {
+  		this.fetchSupplies();
+  		this.fetchGroceries();
+
+  		this.compareSupplyGroceries();
+  	}
+
+  	fetchSupplies(){
+  		//Fetch the available supplies
+	  	this.supplylist = [];
+	  	this.item={name:"Rhabarber",menge:"3 Stangen",wertung:"5", id:"15"};
+	  	this.supplylist.push(this.item);
+	  	this.item={name:"Kandiszucker",menge:"250 g",wertung:"2", id:"23"};
+	  	this.supplylist.push(this.item);
+	  	this.item={name:"Kürbis",menge:"1",wertung:"4", id:"124"};
+	  	this.supplylist.push(this.item);
+  	}
+
+  	fetchGroceries(){
+  		//Fetch the needed groceries
+  		this.neededGroceries = [];
+	  	this.item={name:"Rhabarber",menge:"1 Stangen",wertung:"5", id:"15"};
+	  	this.neededGroceries.push(this.item);
+	  	this.item={name:"Kandiszucker",menge:"350 g",wertung:"2", id:"23"};
+	  	this.neededGroceries.push(this.item);
+	  	this.item={name:"Kürbis",menge:"2",wertung:"4", id:"124"};
+	  	this.neededGroceries.push(this.item);
+	  	this.item={name:"Tee",menge:"1 Päckchen",wertung:"2", id:"24"};
+	  	this.neededGroceries.push(this.item);
+	  	this.item={name:"Vanillezucker",menge:"2 Packungen",wertung:"4", id:"125"};
+	  	this.neededGroceries.push(this.item);
+  	}
+
+  	ionViewDidLoad() {
+  		console.log('Hello EinkaufslistePage Page');
+  	}
+
+  	addGrocery(){
+  	//Save the added grocery
+  	this.newItem={name:this.newGrocery, menge:this.newAmount, wertung:"3", id:"12"};
+  	this.itemlist.push(this.newItem);
+  	//this.storage.set('supplies', JSON.stringify(this.itemlist)).then(this.fetchSupplies2());
   }
+
+  increaseGrocery(id){
+  	//Increase the amount of the chosen grocery
+  	this.currentId = id;
+  	var amount = this.getAmountById(id);
+  	var unit = this.separateUnit(amount);
+  	amount = this.separateAmount(amount);
+  	amount = this.increaseAmount(amount);
+  	this.saveNewAmountById(amount+" "+unit, id);
+  }
+
+  reduceGrocery(id){
+  	//Reduce the amount of the chosen grocery
+  	this.currentId = id;
+  	var amount = this.getAmountById(id);
+  	var unit = this.separateUnit(amount);
+  	amount = this.separateAmount(amount);
+  	amount = this.reduceAmount(amount);
+  	this.saveNewAmountById(amount+" "+unit, id);
+  }
+
+  deleteGrocery(id){
+  	//Delete the amount of the chosen grocery
+  	for (var i = this.itemlist.length - 1; i >= 0; i--) {
+  		if(this.itemlist[i].id == id){
+  			this.itemlist.splice(i, 1);
+  		}
+  	}
+  }
+
+  increaseAmount(amount){
+  	//Increase the given amount
+  	var groceryAmount = +amount;
+  	if(groceryAmount > 1000){
+  		groceryAmount += 500;
+  	}else if(groceryAmount > 100){
+  		groceryAmount += 50;
+  	}else if(groceryAmount > 30){
+  		groceryAmount += 20;
+  	}else if(groceryAmount >= 1){
+  		groceryAmount += 1;
+  	}else if(groceryAmount > 0.2){
+  		groceryAmount += 0.1;
+  	}else if(groceryAmount >= 0.02){
+  		groceryAmount += 0.01;
+  	}
+
+  	groceryAmount = Number((groceryAmount).toFixed(2));
+
+  	return groceryAmount;
+  }
+
+  reduceAmount(amount){
+  	//Increase the given amount
+  	var groceryAmount = +amount;
+  	if(groceryAmount > 1000){
+  		groceryAmount -= 500;
+  	}else if(groceryAmount > 100){
+  		groceryAmount -= 50;
+  	}else if(groceryAmount > 30){
+  		groceryAmount -= 20;
+  	}else if(groceryAmount > 1){
+  		groceryAmount -= 1;
+  	}else if(groceryAmount > 0.2){
+  		groceryAmount -= 0.1;
+  	}else if(groceryAmount > 0.02){
+  		groceryAmount -= 0.01;
+  	}
+
+  	if(groceryAmount < 0){
+  		groceryAmount = 0;
+  	}
+
+  	groceryAmount = Number((groceryAmount).toFixed(2));
+
+  	return groceryAmount;
+  }
+
+  separateAmount(amount){
+  	//Separate the amount value from the amount string
+  	var grocerylist;
+  	grocerylist = amount.split(" ");
+  	return grocerylist[0];
+  }
+
+  separateUnit(amount){
+  	//Separate the unit from the amount string
+  	this.grocerylist = amount.split(" ");
+  	if(this.grocerylist[1] != undefined){
+  		return this.grocerylist[1];
+  	}else{
+  		return "";
+  	}
+  }
+
+  saveNewAmountById(newAmount, id){
+  	//Save the new amount
+  	for (var i = this.itemlist.length - 1; i >= 0; i--) {
+  		if(this.itemlist[i].id == id){
+  			this.itemlist[i].menge = newAmount;
+  			return;
+  		}
+  	}
+  }
+
+  getAmountById(id){
+  	//Return the amount of the given supply 
+  	for (var i = this.itemlist.length - 1; i >= 0; i--) {
+  		if(this.itemlist[i].id == id){
+  			return this.itemlist[i].menge;
+  		}
+  	}
+  }
+
+  compareSupplyGroceries(){
+  	//Compare the available supplies and needed groceries
+  	//Return a list containing the missing groceries
+  	this.missingGroceries = [];
+  	let missingGrocery;
+  	let subtractedAmount;
+
+  	//Iterate through the needed groceries and the available supplies to find 
+  	//matching ids and check whether the given amount is bigger than the needed amount
+  	//If the needed amount is bigger calculate the missing amount and save it in the
+  	//missingGroceries list
+  	for (var i = this.neededGroceries.length - 1; i >= 0; i--) {
+  		for (var j = this.supplylist.length - 1; j >= 0; j--) {
+
+  			var id1 = +this.supplylist[j].id;
+  			var id2 = +this.neededGroceries[i].id;
+  			console.log("Vergleich: ");
+  			console.log(this.supplylist[j]);
+  			console.log(this.neededGroceries[i]);
+  			if(id1 === id2){
+
+  				subtractedAmount = this.subtractTwoAmounts(this.neededGroceries[i].menge, this.supplylist[j].menge);
+  				if(this.separateAmount(subtractedAmount) > 0){
+
+  					missingGrocery = this.neededGroceries[j];
+  					missingGrocery.menge = subtractedAmount;
+  					this.missingGroceries.push(missingGrocery);
+  				}
+  				//Break the current loop
+  				j = -1;
+  			}
+  		}
+  	}
+  	this.itemlist = this.missingGroceries;
+  	console.log(this.itemlist);
+  }
+
+  subtractTwoAmounts(amount1, amount2){
+  	var amount, unit;
+  	
+  	unit = this.separateUnit(amount1);
+  	amount = this.separateAmount(amount1) - this.separateAmount(amount2);
+
+  	return amount + " " + unit;
+  }
+
 
 }
--- ionic_app/src/pages/vorraete/vorraete.ts
+++ ionic_app/src/pages/vorraete/vorraete.ts
@@ -1,22 +1,190 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,NavParams } from 'ionic-angular';
+//import { Storage} from '@ionic/storage';
+
 
 /*
   Generated class for the Vorraete page.
 
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
-*/
-@Component({
-  selector: 'page-vorraete',
-  templateUrl: 'vorraete.html'
-})
-export class VorraetePage {
+  */
+  @Component({
+  	selector: 'page-vorraete',
+  	templateUrl: 'vorraete.html'
+  })
+  export class VorraetePage {
 
-  constructor(public navCtrl: NavController) {}
+  	neuerVorrat:any;
+  	neueMenge:any;
+  	supplies:any;
+  	item:any;
+  	item2:any;
+  	item3:any;
+  	newItem:any;
+  	currentId:any;
+  	itemlist:Array<any>;
+  	unitlist: string[] = ["mg", "g", "kg", "l", "liter","Stangen"];
+  	supplylist: string[] = [];
+  	unit:any;
+
+  	
+
+  	constructor(public navCtrl: NavController/*, public storage:Storage*/) {
+  		this.fetchSupplies();
+  		//this.fetchSupplies2();
+
+  	}
+
+
+/*
+  	fetchSupplies2(){
+
+  		this.storage.get('supplies').then(function(data){
+  			if (data != null){
+  				this.supplies = JSON.parse(data);
+  				console.log("data: "+data);
+  				console.log("JSONdata: "+JSON.parse(data));
+  			}else{
+  				this.loadDefaultQuestions();
+  			}
+  		});
+  		console.log("supplies: "+this.supplies);
+  	}*/
+
+  	fetchSupplies(){
+  	//Fetch the saved supplies
+  	this.itemlist = [];
+  	this.item={name:"Rhabarber",menge:"3 Stangen",wertung:"5", id:"15"};
+  	this.item2={name:"Kandiszucker",menge:"250 g",wertung:"2", id:"23"};
+  	this.item3={name:"Kürbis",menge:"1",wertung:"4", id:"124"};
+  	this.itemlist.push(this.item);
+  	this.itemlist.push(this.item2);
+  	this.itemlist.push(this.item3);
+  }
 
   ionViewDidLoad() {
-    console.log('Hello VorraetePage Page');
+  	console.log("itemlist: "+this.itemlist);
+  }
+
+  addVorrat(){
+  	//Save the added supply
+  	this.newItem={name:this.neuerVorrat, menge:this.neueMenge, wertung:"3", id:"12"};
+  	this.itemlist.push(this.newItem);
+  	//this.storage.set('supplies', JSON.stringify(this.itemlist)).then(this.fetchSupplies2());
+  }
+
+  increaseSupply(id){
+  	//Increase the amount of the chosen supply
+  	this.currentId = id;
+  	var amount = this.getAmountById(id);
+  	var unit = this.separateUnit(amount);
+  	amount = this.separateAmount(amount);
+  	amount = this.increaseAmount(amount);
+  	this.saveNewAmountById(amount+" "+unit, id);
+  }
+
+  reduceSupply(id){
+  	//Reduce the amount of the chosen supply
+  	this.currentId = id;
+  	var amount = this.getAmountById(id);
+  	var unit = this.separateUnit(amount);
+  	amount = this.separateAmount(amount);
+  	amount = this.reduceAmount(amount);
+  	this.saveNewAmountById(amount+" "+unit, id);
+  }
+
+  deleteSupply(id){
+  	//Delete the amount of the chosen supply
+  	for (var i = this.itemlist.length - 1; i >= 0; i--) {
+  		if(this.itemlist[i].id == id){
+  			this.itemlist.splice(i, 1);
+  		}
+  	}
+  }
+
+  increaseAmount(amount){
+  	//Increase the given amount
+  	var supplyAmount = +amount;
+  	if(supplyAmount > 1000){
+  		supplyAmount += 500;
+  	}else if(supplyAmount > 100){
+  		supplyAmount += 50;
+  	}else if(supplyAmount > 30){
+  		supplyAmount += 20;
+  	}else if(supplyAmount >= 1){
+  		supplyAmount += 1;
+  	}else if(supplyAmount > 0.2){
+  		supplyAmount += 0.1;
+  	}else if(supplyAmount > 0.02){
+  		supplyAmount += 0.01;
+  	}
+
+  	supplyAmount = Number((supplyAmount).toFixed(2));
+
+  	return supplyAmount;
+  }
+
+  reduceAmount(amount){
+  	//Increase the given amount
+  	var supplyAmount = +amount;
+  	if(supplyAmount > 1000){
+  		supplyAmount -= 500;
+  	}else if(supplyAmount > 100){
+  		supplyAmount -= 50;
+  	}else if(supplyAmount > 30){
+  		supplyAmount -= 20;
+  	}else if(supplyAmount > 1){
+  		supplyAmount -= 1;
+  	}else if(supplyAmount > 0.2){
+  		supplyAmount -= 0.1;
+  	}else if(supplyAmount > 0.02){
+  		supplyAmount -= 0.01;
+  	}
+
+  	if(supplyAmount < 0){
+  		supplyAmount = 0;
+  	}
+
+  	supplyAmount = Number((supplyAmount).toFixed(2));
+
+  	return supplyAmount;
+  }
+
+  separateAmount(amount){
+  	//Separate the amount value from the amount string
+  	var supplylist;
+  	supplylist = amount.split(" ");
+  	return supplylist[0];
+  }
+
+  separateUnit(amount){
+  	//Separate the unit from the amount string
+  	this.supplylist = amount.split(" ");
+  	if(this.supplylist[1] != undefined){
+  		return this.supplylist[1];
+  	}else{
+  		return "";
+  	}
+  }
+
+  saveNewAmountById(newAmount, id){
+  	//Save the new amount
+  	for (var i = this.itemlist.length - 1; i >= 0; i--) {
+  		if(this.itemlist[i].id == id){
+  			this.itemlist[i].menge = newAmount;
+  			return;
+  		}
+  	}
+  }
+
+  getAmountById(id){
+  	//Return the amount of the given supply 
+  	for (var i = this.itemlist.length - 1; i >= 0; i--) {
+  		if(this.itemlist[i].id == id){
+  			return this.itemlist[i].menge;
+  		}
+  	}
   }
 
 }
